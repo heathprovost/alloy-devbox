760e7e69b7b4c0e4ad10c0dace4c3021fc4addbc
Heath Provost <heath_provost@stuller.com>
2023-11-23 16:11:20 -0600

    fix sudo

--- devbox.sh
+++ devbox.sh
@@ -89,7 +89,7 @@ function install_node () {
   if [ ! -d "${HOME}/.nvm/.git" ]; then
     # download and run install script directly from nvm github repo
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
-    ENV_UPDATED=true
+    ENV_UPDATED="true"
   fi
 
   # update current shell with exports needed to run nvm commands
@@ -136,7 +136,7 @@ function install_dotnet_sdk() {
   # append exports to .bashrc if needed
   if ! grep -qc '$HOME/.dotnet' "$HOME/.bashrc"; then
     printf '\n# dotnet exports\nexport DOTNET_ROOT="$HOME/.dotnet"\nexport PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools\n' >> "$HOME/.bashrc"
-    ENV_UPDATED=true
+    ENV_UPDATED="true"
   fi
 
   # update current shell with exports needed to run dotnet commands
@@ -292,7 +292,7 @@ function setup() {
   execute_and_wait 'meteor_deps'
 
   printf "${ANSI_GREEN}${CHECK_SYMBOL}${ANSI_NC} Done!\n\n"
-  if [ "$ENV_UPDATED" = true ]; then
+  if [ "${ENV_UPDATED}" = "true" ]; then
     printf "✨ ${ANSI_YELLOW}Environment has been updated. Run ${ANSI_BLUE}'source ~/.bashrc'${ANSI_YELLOW} to reload your current shell session${ANSI_NC}\n"
   fi
 }
